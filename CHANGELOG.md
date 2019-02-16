8fd6b234c639599ec0d5cb71ddef9cf50f1035e3
Daniele Pallastrelli <daniele.pallastrelli@gmail.com>
2019-02-16 12:04:30 +0100

    Fixes #29 (check for invalid stream in CliFileSession)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ Current Version: 1.0-alpha
 - Fix Clang 3.6 - 7.0 undefined reference bug
 - Fix infinite loop on EoF
 - Fix issue #27 (odd history behaviour): the history now works like in bash
+- Fix issue #28 (check for invalid stream in CliFileSession class)
 - Add doxygen basic configuration
 - Add session recordings in README file
 - Add CHANGELOG.md, CODE_OF_CONDUCT.md, CONTRIBUTING.md files
--- cli/clifilesession.h
+++ cli/clifilesession.h
@@ -32,6 +32,7 @@
 
 #include <string>
 #include <iostream>
+#include <stdexcept> // std::invalid_argument
 #include "cli.h" // CliSession
 
 namespace cli
@@ -40,12 +41,15 @@ namespace cli
 class CliFileSession : public CliSession
 {
 public:
+	/// @throw std::invalid_argument if @c _in or @c out are invalid streams
     CliFileSession(Cli& cli, std::istream& _in=std::cin, std::ostream& out=std::cout) :
         CliSession(cli, out, 1),
         exit(false),
         in(_in)
     {
-        ExitAction(
+		if (!in.good())	throw std::invalid_argument("istream invalid");
+		if (!out.good()) throw std::invalid_argument("ostream invalid");
+		ExitAction(
             [this](std::ostream&)
             {
                 exit = true;
@@ -58,6 +62,8 @@ public:
         {
             Prompt();
             std::string line;
+			if (!in.good())
+				Exit();
             std::getline(in, line);
             if (in.eof())
                 Exit();
