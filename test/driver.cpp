639a2c90f23a49a24c943f258f6dea01b13985b1
Daniele Pallastrelli <5451767+daniele77@users.noreply.github.com>
2020-02-28 11:45:42 +0100

    Update driver.cpp

--- test/driver.cpp
+++ test/driver.cpp
@@ -29,4 +29,5 @@
 
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE CliTest
-#include <boost/test/unit_test.hpp>
+// #include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
