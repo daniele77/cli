daniele77/cli

commit 0a4d86089e6d92c1156708189d8aca18c743b63d

Specifies signed for char parameters

include/cli/detail/genericasioremotecli.h
@@ -219,13 +219,13 @@ protected:
 
     void OnDataReceived(const std::string& _data) override
     {
-        for (char c: _data)
+        for (auto c: _data)
             Consume(c);
     }
 
 private:
 
-    void Consume(char c)
+    void Consume(signed char c)
     {
         if (escape)
         {
@@ -244,7 +244,7 @@ private:
         }
     }
 
-    void Data(char c)
+    void Data(signed char c)
     {
         switch(state)
         {
@@ -407,7 +407,7 @@ private:
         this -> OutStream() << answer << std::flush;
     }
 protected:
-    virtual void Output(char c)
+    virtual void Output(signed char c)
     {
         #ifdef CLI_TELNET_TRACE
         std::cout << "data: " << static_cast<int>(c) << std::endl;
@@ -466,7 +466,7 @@ protected:
         Prompt();
     }
 
-    void Output(char c) override
+    void Output(signed char c) override // NB: C++ does not specify wether char is signed or unsigned
     {
         switch(step)
         {

commit 8c8cd992f52bead09c0fd89ca28ed692346e6f0a

bugfix: revert 0a4d8608 signed char on data reception
Commit 0a4d8608 caused received characters to be forcibly converted from
unsigned -> signed on some platforms, and so incoming byte values >127 (such as
telnet escape sequences) were getting converted to negative values. The problem
is that the enum containing the escape sequence constants has wider underlying
type (int) that can store the large unsigned value (such as, 0xFF). This caused
telnet escape sequence comparisons to fail (such as: -128 != 255).

The end result was that telnet connections close immediately upon connection
(for busybox telnet client, at least).

This change standardizes on unsigned char as the reception char type, ensuring
there is sufficient width for the telnet escape sequences.

include/cli/detail/genericasioremotecli.h
@@ -173,7 +173,7 @@ protected:
     for a list of telnet options
     */
 
-    enum
+    enum : unsigned char
     {
         SE = '\x0F0',                  // End of subnegotiation parameters.
         NOP = '\x0F1',                 // No operation.
@@ -225,7 +225,7 @@ protected:
 
 private:
 
-    void Consume(signed char c)
+    void Consume(unsigned char c)
     {
         if (escape)
         {
@@ -244,7 +244,7 @@ private:
         }
     }
 
-    void Data(signed char c)
+    void Data(unsigned char c)
     {
         switch(state)
         {